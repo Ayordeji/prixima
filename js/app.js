53c50ecbe850d5ad6d796ebab63df2f27842e475
Ayodeji <ayoeluyemi2002@gmail.com>
2023-03-28 00:43:06 +0100

    Porfolio section added

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   //Owl
   $(".hero-slider").owlCarousel({
     loop: true,
-    nav: true,
+    margin: 0,
     items: 1,
     dots: false,
     navText: ["PREV", "NEXT"],
@@ -13,10 +13,29 @@ $(document).ready(function () {
       0: {
         nav: false,
       },
+      768: {
+        nav: true,
+      },
+    },
+  });
 
-      768: {},
-
-      1000: {},
+  $("#projects-slider").owlCarousel({
+    loop: true,
+    nav: false,
+    items: 2,
+    dots: true,
+    smartSpeed: 600,
+    center: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+        margin: 8,
+      },
     },
   });
 });
